9295ab0241737435d1cc14ffe974d558f25281c3
wangxianxi <1853cong@sina.cn;git config --global user.email 1853cong@sina.cn>
2016-09-24 14:08:14 +0800

    shopcart

---
+++ shopping-cart/data.js
@@ -0,0 +1,17 @@
+var data=[
+	{
+		name:'手机',
+		price:1231,
+		count:1
+	},
+	{
+		name:'电脑',
+		price:1231,
+		count:1
+	},
+	{
+		name:'平板',
+		price:1231,
+		count:1
+	}
+]
---
+++ shopping-cart/note.md
@@ -0,0 +1,11 @@
+
+###	魔术变量
+	$index在渲染的过程中，获得当前的索引
+### 模版渲染
+	template 
+		v-if
+		v-else
+### 事件绑定
+	@click=""
+### computed
+	计算的数值不需要再data中预先定义
